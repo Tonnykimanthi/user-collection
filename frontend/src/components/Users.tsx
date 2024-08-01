0ce3541f41b37289b07ec678d38d9141a4405ee0
Tony Km <kimaxhanthony9@gmail.com>
2024-08-01 22:09:08 +0300

    Create users context

--- frontend/src/components/Users.tsx
+++ frontend/src/components/Users.tsx
@@ -1,24 +1,30 @@
+
+
 const Users = () => {
+
   return (
     <main>
-      <table className="mt-5 w-full text-center">
+      <table className="mt-10 w-full text-center">
         <tr className="bg-slate-700 text-white [&>th]:py-3">
           <th>FirstName</th>
           <th>LastName</th>
-          <th>Email</th>
+          <th>Email address</th>
           <th>Password</th>
+          <th>created At</th>
         </tr>
-        <tr className="odd:bg-secondary even:bg-white [&>td]:border [&>td]:border-ssecondary [&>td]:py-2">
+        <tr className="odd:bg-secondary even:bg-white [&>td]:border [&>td]:border-secondary [&>td]:py-2">
           <td>Antonio</td>
           <td>Mutua</td>
           <td>kimaxh@gmail.com</td>
           <td>123abcd</td>
+          <td>20 mins ago</td>
         </tr>
-        <tr className="odd:bg-secondary even:bg-white [&>td]:border [&>td]:border-ssecondary [&>td]:py-2">
+        <tr className="odd:bg-secondary even:bg-white [&>td]:border [&>td]:border-secondary [&>td]:py-2">
           <td>Antonio</td>
           <td>Mutua</td>
           <td>tonnykimanthi@gmail.com</td>
           <td>123abcd</td>
+          <td>20 mins ago</td>
         </tr>
       </table>
     </main>
