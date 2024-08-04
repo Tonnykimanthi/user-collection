ec62103deeb67b7dc9a9da05c91f8856da6340ab
Tony Km <kimaxhanthony9@gmail.com>
2024-08-04 22:19:55 +0300

    Add mobile navbar

---
+++ frontend/src/components/MobileNavbar.tsx
@@ -0,0 +1,62 @@
+import { NavLink } from "react-router-dom";
+import { IoCloseOutline } from "react-icons/io5";
+
+// DOES NOT WORK AT THE MOMENT
+type MobileNavbarProps = {
+  mobileNavbarIsActive: boolean;
+  setMobileNavbarIsActive: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const MobileNavbar = ({
+  mobileNavbarIsActive,
+  setMobileNavbarIsActive,
+}: MobileNavbarProps) => {
+  return (
+    <nav
+      className={`absolute bottom-0 left-0 top-0 flex w-0 items-center justify-center transition-all bg-primary-dark ${mobileNavbarIsActive ? "w-full" : "w-0"}`}
+    >
+      <button
+        className="absolute right-5 top-5"
+        onClick={() => setMobileNavbarIsActive(false)}
+      >
+        <IoCloseOutline className="size-7 transition hover:scale-110" />
+      </button>
+      <ul className="flex flex-col gap-y-4 text-lg font-medium text-white">
+        <NavLink
+          to={"/"}
+          className={({ isActive }) =>
+            isActive ? "underline underline-offset-4" : ""
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to={"about"}
+          className={({ isActive }) =>
+            isActive ? "underline underline-offset-4" : ""
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to={"contact"}
+          className={({ isActive }) =>
+            isActive ? "underline underline-offset-4" : ""
+          }
+        >
+          Contact
+        </NavLink>
+        <NavLink
+          to={"https://portfolio-fawn-six-94.vercel.app/"}
+          className={({ isActive }) =>
+            isActive ? "underline underline-offset-4" : ""
+          }
+        >
+          Portfolio
+        </NavLink>
+      </ul>
+    </nav>
+  );
+};
+
+export default MobileNavbar;
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,21 @@
 import { NavLink } from "react-router-dom";
 import useUsersContext from "../hooks/useUsersContext";
+import MobileNavbar from "./MobileNavbar";
+import { useState } from "react";
+import { RiMenu2Fill } from "react-icons/ri";
 
 const Navbar = () => {
   const { setFormIsActive } = useUsersContext();
+  const [mobileNavbarIsActive, setMobileNavbarIsActive] = useState(false);
   return (
     <header className="flex items-center bg-primary-default p-2 text-white">
-      <nav className="flex w-full items-center justify-center">
+      <button
+        onClick={() => setMobileNavbarIsActive(true)}
+        className="sm:hidden"
+      >
+        <RiMenu2Fill className="size-6" />
+      </button>
+      <nav className="flex w-full items-center justify-center max-sm:hidden">
         <ul className="flex gap-x-4 text-lg font-medium text-secondary">
           <NavLink
             to={"/"}
@@ -42,13 +52,17 @@ const Navbar = () => {
         </ul>
       </nav>
       <button
-        className="ml-auto w-52 rounded bg-secondary px-4 py-1 text-black transition hover:bg-secondary/90"
+        className="ml-auto w-52 rounded bg-secondary px-4 py-1 text-black transition hover:bg-secondary/90 max-sm:w-40"
         onClick={() => {
           setFormIsActive((isActive) => !isActive);
         }}
       >
         Create new user
       </button>
+      <MobileNavbar
+        mobileNavbarIsActive={mobileNavbarIsActive}
+        setMobileNavbarIsActive={setMobileNavbarIsActive}
+      />
     </header>
   );
 };
