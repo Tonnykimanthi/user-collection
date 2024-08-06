af45281e09f6065c173c1e9a79867dd3a122d8c0
Tony Km <kimaxhanthony9@gmail.com>
2024-08-06 18:45:51 +0300

    Close modals by clicking outside them

--- frontend/src/components/CreateUserForm.tsx
+++ frontend/src/components/CreateUserForm.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useRef } from "react";
 import useUsersContext from "../hooks/useUsersContext";
+import { IoCloseOutline } from "react-icons/io5";
 
 const CreateUserForm = () => {
   const {
@@ -12,6 +14,7 @@ const CreateUserForm = () => {
     setLastName,
     setEmail,
   } = useUsersContext();
+  const formElRef = useRef<HTMLFormElement | null>(null);
 
   const handleCreateUser = async () => {
     try {
@@ -40,42 +43,70 @@ const CreateUserForm = () => {
     }
   };
 
+  useEffect(() => {
+    const handleCloseForm = (e: Event) => {
+      if (!formElRef.current?.contains(e.target as HTMLFormElement)) {
+        setFormIsActive(false);
+      }
+    };
+
+    document.body.addEventListener("mousedown", handleCloseForm);
+
+    return () => {
+      document.body.addEventListener("mousedown", handleCloseForm);
+    };
+  });
+
   return (
-    <form
-      className={`absolute left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white px-5 py-8 text-lg [&>input]:w-full [&>input]:border [&>input]:p-1.5 [&>input]:outline-none [&>label]:font-medium ${formIsActive ? "-translate-y-1/2" : "-translate-y-[200%]"} transition`}
-      onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        handleCreateUser();
-      }}
+    <div
+      className={`absolute bottom-0 left-0 right-0 top-0 bg-black/50 transition ${formIsActive ? "" : "scale-0"}`}
     >
-      <label>First name:</label> <br />
-      <input
-        type="text"
-        value={firstName}
-        onChange={(e) => {
-          setFirstName(e.target.value);
+      <form
+        ref={formElRef}
+        className={`absolute left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white px-5 pb-8 pt-10 text-lg duration-500 [&>input]:w-full [&>input]:border [&>input]:p-1.5 [&>input]:outline-none [&>label]:font-medium ${formIsActive ? "-translate-y-1/2" : "-translate-y-[200%]"} transition`}
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+          e.preventDefault();
+          handleCreateUser();
         }}
-      />
-      <label className="mt-4 block">Last name:</label>
-      <input
-        type="text"
-        value={lastName}
-        onChange={(e) => {
-          setLastName(e.target.value);
-        }}
-      />
-      <label className="mt-4 block">Email:</label>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      />
-      <button className="mt-5 w-full bg-primary-default py-2 text-white transition hover:bg-primary-dark">
-        Create
-      </button>
-    </form>
+      >
+        <button
+          className="absolute right-2 top-2 transition hover:scale-110"
+          onClick={(e) => {
+            e.preventDefault();
+            setFormIsActive(false);
+          }}
+        >
+          <IoCloseOutline className="size-7" />
+        </button>
+        <label>First name:</label> <br />
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => {
+            setFirstName(e.target.value);
+          }}
+        />
+        <label className="mt-4 block">Last name:</label>
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+        />
+        <label className="mt-4 block">Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <button className="mt-5 w-full bg-primary-default py-2 text-white transition hover:bg-primary-dark">
+          Create
+        </button>
+      </form>
+    </div>
   );
 };
 
--- frontend/src/components/UserModal.tsx
+++ frontend/src/components/UserModal.tsx
@@ -1,6 +1,6 @@
 import { IoCloseOutline } from "react-icons/io5";
 import useUsersContext from "../hooks/useUsersContext";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DeleteUserModal from "./DeleteUserModal";
 
 const UserModal = () => {
@@ -16,15 +16,30 @@ const UserModal = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
+  const formElRef = useRef<HTMLFormElement | null>(null);
 
   useEffect(() => {
     if (activeUser !== null) {
       const user = state.users[activeUser];
+      // const body = document.querySelector("body");
       if (user) {
         setFirstName(user.firstName);
         setLastName(user.lastName);
         setEmail(user.email);
       }
+      // Close modal by clicking outside it
+      const handleCloseModal = (e: Event) => {
+        if (!formElRef.current?.contains(e.target as Node)) {
+          setUserModalIsOpen(false);
+          setActiveUser(null);
+        }
+      };
+
+      document.body?.addEventListener("mousedown", handleCloseModal);
+
+      return () => {
+        document.body?.removeEventListener("mousedown", handleCloseModal);
+      };
     }
   }, [activeUser, state.users]);
 
@@ -34,7 +49,6 @@ const UserModal = () => {
     setUserModalIsOpen(false);
     setActiveUser(null);
   };
-
 
   const handleUpdateUser = async () => {
     const updatedUser = {
@@ -71,68 +85,73 @@ const UserModal = () => {
   };
 
   return (
-    <form
-      className={`absolute left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white px-5 pb-5 pt-10 text-lg shadow-lg transition [&>input]:w-full [&>input]:border [&>input]:p-1.5 [&>input]:outline-none [&>label]:font-medium ${userModalIsOpen ? "" : "scale-0"}`}
+    <div
+      className={`absolute bottom-0 left-0 right-0 top-0 bg-black/50 transition-all duration-1000 ${userModalIsOpen ? "" : "scale-0"}`}
     >
-      <button
-        className="absolute right-2 top-2"
-        onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-          e.preventDefault();
-          handleModalIsOpen();
-        }}
+      <form
+        ref={formElRef}
+        className={`absolute left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white px-5 pb-5 pt-10 text-lg shadow-lg transition [&>input]:w-full [&>input]:border [&>input]:p-1.5 [&>input]:outline-none [&>label]:font-medium`}
       >
-        <IoCloseOutline className="size-7" />
-      </button>
-      <label>First name:</label> <br />
-      <input
-        type="text"
-        value={firstName}
-        onChange={(e) => {
-          setFirstName(e.target.value);
-        }}
-      />
-      <label className="mt-4 block">Last name:</label>
-      <input
-        type="text"
-        value={lastName}
-        onChange={(e) => {
-          setLastName(e.target.value);
-        }}
-      />
-      <label className="mt-4 block">Email:</label>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      />
-      <section className="mt-5 flex justify-evenly font-medium">
         <button
-          className="rounded bg-primary-light px-8 py-2 text-white transition hover:bg-primary-default"
-          onClick={(e) => {
+          className="absolute right-2 top-2 hover:scale-110"
+          onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
             e.preventDefault();
-            handleUpdateUser();
             handleModalIsOpen();
           }}
         >
-          Update User
+          <IoCloseOutline className="size-7" />
         </button>
-        <button
-          className="rounded bg-slate-700 px-8 py-2 text-white transition"
-          onClick={(e) => {
-            e.preventDefault();
-            setDeleteModalIsOpen(true);
+        <label>First name:</label> <br />
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => {
+            setFirstName(e.target.value);
           }}
-        >
-          Delete User
-        </button>
-      </section>
-      <DeleteUserModal
-        id={state.users[activeUser]._id}
-        user={state.users[activeUser]}
-      />
-    </form>
+        />
+        <label className="mt-4 block">Last name:</label>
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+        />
+        <label className="mt-4 block">Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <section className="mt-5 flex justify-evenly font-medium">
+          <button
+            className="rounded bg-primary-light px-8 py-2 text-white transition hover:bg-primary-default"
+            onClick={(e) => {
+              e.preventDefault();
+              handleUpdateUser();
+              handleModalIsOpen();
+            }}
+          >
+            Update User
+          </button>
+          <button
+            className="rounded bg-slate-700 px-8 py-2 text-white transition"
+            onClick={(e) => {
+              e.preventDefault();
+              setDeleteModalIsOpen(true);
+            }}
+          >
+            Delete User
+          </button>
+        </section>
+        <DeleteUserModal
+          id={state.users[activeUser]._id}
+          user={state.users[activeUser]}
+        />
+      </form>
+    </div>
   );
 };
 
