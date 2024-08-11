8c38b17b0a0e92b5747ed6da2f8bfe186053df4c
Tony Km <kimaxhanthony9@gmail.com>
2024-08-11 17:40:04 +0300

    Add Contact page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import CreateUserForm from "./components/CreateUserForm";
 import Navbar from "./components/Navbar";
 import UserModal from "./components/UserModal";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
