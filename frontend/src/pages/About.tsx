99b21bb4d77d0f9a40f2bff25a05186d906256f6
Tony Km <kimaxhanthony9@gmail.com>
2024-08-14 17:50:53 +0300

    Add svg for background

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,18 +1,17 @@
 const About = () => {
   return (
-    <div className="h-[100vh] relative">
-      <div className="h-full bg-[url('https://plus.unsplash.com/premium_photo-1678566154673-a728037f3f00?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8d2ViJTIwZGV2ZWxvcG1lbnR8ZW58MHx8MHx8fDA%3D')] bg-cover bg-no-repeat blur-sm"></div>
-      <section className="absolute bg-black/10 text-white left-0 right-0 top-0 text-center p-10 max-sm:p-5">
-        <h2 className="text-6xl font-bold italic max-sm:text-4xl border-b border-white/30 pb-4">About</h2>
-        <p className="mt-4 text-lg font-medium max-sm:text-base">
-          The website is created for the purpose of saving your credentials
-          which a person has used to login to the specific website. The
-          credentials includes your, first name or username, last name and also
-          your email address. Your credentials are saved to the database for
-          future reference.
-        </p>
-      </section>
-    </div>
+    <section className="p-10 text-center text-white max-sm:p-5">
+      <h2 className="border-b border-white/30 pb-4 text-6xl font-bold italic max-sm:text-4xl">
+        About
+      </h2>
+      <p className="mt-4 text-lg font-medium max-sm:text-base">
+        Our mission is to make managing your online credentials as simple and
+        secure as possible. Our platform is designed to store and organize your
+        login details for various websites, ensuring you never have to worry
+        about remembering all those usernames, passwords, and email addresses
+        again.
+      </p>
+    </section>
   );
 };
 
--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -1,46 +1,42 @@
 const Contact = () => {
   return (
-    <div className="relative h-screen">
-      <div className="h-full bg-[url('https://images.unsplash.com/photo-1684569547016-d0bdc13d2c50?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHdlYiUyMGRldmVsb3BtZW50fGVufDB8fDB8fHww')] bg-cover bg-no-repeat"></div>
-
-      <section className="absolute left-1/2 top-1/2 w-full max-w-lg -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white/50 p-5 shadow-md backdrop-blur-lg">
-        <h2 className="text-center text-3xl">Contact me</h2>
-        <form className="mt-2 grid grid-cols-2 gap-5 text-lg">
-          <div>
-            <label>First Name</label>
-            <input
-              type="text"
-              className="w-full rounded border p-1.5 outline-none transition duration-300 focus:border-primary-dark"
-            />
-          </div>
-          <div>
-            <label>Last Name</label>
-            <input
-              type="text"
-              className="w-full rounded border p-1.5 outline-none transition duration-300 focus:border-primary-dark"
-            />
-          </div>
-          <div className="col-span-2">
-            <label>Email</label>
-            <input
-              type="email"
-              className="w-full rounded border p-1.5 outline-none transition duration-300 focus:border-primary-dark"
-            />
-          </div>
-          <div className="col-span-2">
-            <label>Subject</label>
-            <input
-              type="text"
-              className="w-full rounded border p-1.5 outline-none transition duration-300 focus:border-primary-dark"
-            />
-          </div>
-          <div className="col-span-2">
-            <label>Message</label>
-            <textarea className="w-full rounded border p-1.5 outline-none transition duration-300 focus:border-primary-dark"></textarea>
-          </div>
-        </form>
-      </section>
-    </div>
+    <section className="mx-auto mt-10 w-full max-w-lg rounded-lg bg-white/10 p-5 text-white shadow-md backdrop-blur-lg">
+      <h2 className="text-center text-3xl">Contact me</h2>
+      <form className="mt-2 grid grid-cols-2 gap-5 text-lg [&>div>input]:bg-transparent/10">
+        <div>
+          <label>First Name</label>
+          <input
+            type="text"
+            className="w-full rounded border border-primary-dark p-1.5 outline-none transition duration-300 focus:border-primary-default"
+          />
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input
+            type="text"
+            className="w-full rounded border border-primary-dark p-1.5 outline-none transition duration-300 focus:border-primary-default"
+          />
+        </div>
+        <div className="col-span-2">
+          <label>Email</label>
+          <input
+            type="email"
+            className="w-full rounded border border-primary-dark p-1.5 outline-none transition duration-300 focus:border-primary-default"
+          />
+        </div>
+        <div className="col-span-2">
+          <label>Subject</label>
+          <input
+            type="text"
+            className="w-full rounded border border-primary-dark p-1.5 outline-none transition duration-300 focus:border-primary-default"
+          />
+        </div>
+        <div className="col-span-2">
+          <label>Message</label>
+          <textarea className="w-full rounded border border-primary-dark bg-transparent/10 p-1.5 outline-none transition duration-300 focus:border-primary-default"></textarea>
+        </div>
+      </form>
+    </section>
   );
 };
 
